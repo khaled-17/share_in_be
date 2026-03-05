8fc855269a12b96ff28757965961f377050ffe02
ram <ram@example.com>
2026-03-05 05:24:42 +0200

    fix: enable CORS to fix cross-origin errors from frontend

--- dist/main.js
+++ dist/main.js
@@ -41,6 +41,7 @@ const fs = __importStar(require("fs"));
 const path = __importStar(require("path"));
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, transform: true }));
     const packageJsonPath = path.join(process.cwd(), 'package.json');
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   // Read package.json version
